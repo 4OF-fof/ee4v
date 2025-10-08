1c383310f1b5502925c6b3e41211003f4e1b68b9
4OF <404.fof.nf@gmail.com>
2025-10-08 10:13:28 +0900

    fix: cant build avatar

--- Runtime/ObjectStyle.cs
+++ Runtime/ObjectStyle.cs
@@ -9,26 +9,4 @@ namespace _4OF.ee4v.Runtime {
 
         [Tooltip("Icon")] public Texture icon;
     }
-
-    [CanEditMultipleObjects]
-    [CustomEditor(typeof(ObjectStyleComponent))]
-    public class ObjectStyleComponentEditor : Editor {
-        private void OnEnable() {
-            serializedObject.FindProperty("m_Script");
-        }
-
-        public override void OnInspectorGUI() {
-            serializedObject.Update();
-            var prop = serializedObject.GetIterator();
-            var enterChildren = true;
-            while (prop.NextVisible(enterChildren)) {
-                enterChildren = false;
-                if (prop.name == "m_Script")
-                    continue;
-                EditorGUILayout.PropertyField(prop, true);
-            }
-
-            serializedObject.ApplyModifiedProperties();
-        }
-    }
 }
