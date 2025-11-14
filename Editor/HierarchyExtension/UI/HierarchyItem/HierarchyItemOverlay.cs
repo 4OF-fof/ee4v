9bae42b8258056dcd8a27533b336a04eb35a6684
4OF <404.fof.nf@gmail.com>
2025-11-14 20:09:38 +0900

    lint

--- Editor/HierarchyExtension/UI/HierarchyItem/HierarchyItemOverlay.cs
+++ Editor/HierarchyExtension/UI/HierarchyItem/HierarchyItemOverlay.cs
@@ -1,6 +1,5 @@
 ﻿using _4OF.ee4v.Core.Data;
 using _4OF.ee4v.Core.UI;
-using _4OF.ee4v.HierarchyExtension.Service;
 using _4OF.ee4v.HierarchyExtension.UI.HierarchyItem.Window;
 using _4OF.ee4v.HierarchyExtension.Utility;
 using _4OF.ee4v.Runtime;
