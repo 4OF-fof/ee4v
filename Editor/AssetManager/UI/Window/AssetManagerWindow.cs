1cb453973d4544205be9ecc2cd925bf59dfe506e
4OF <404.fof.nf@gmail.com>
2025-11-15 21:53:52 +0900

    revert

--- Editor/AssetManager/UI/Window/AssetManagerWindow.cs
+++ Editor/AssetManager/UI/Window/AssetManagerWindow.cs
@@ -1,11 +1,8 @@
 ﻿using _4OF.ee4v.AssetManager.Service;
 using UnityEditor;
-using _4OF.ee4v.AssetManager.UI.Window._Component;
 
 namespace _4OF.ee4v.AssetManager.UI.Window {
     public class AssetManagerWindow: EditorWindow {
-        private MainContent _mainContent;
-        private MainContentController _mainContentController;
         [MenuItem("ee4v/Asset Manager")]
         public static void ShowWindow() {
             var window = GetWindow<AssetManagerWindow>("Asset Manager");
@@ -14,18 +11,6 @@ namespace _4OF.ee4v.AssetManager.UI.Window {
         }
 
         private void CreateGUI() {
-            rootVisualElement.Clear();
-            var toolbar = new Toolbar();
-            rootVisualElement.Add(toolbar);
-
-            _mainContent = new MainContent();
-            rootVisualElement.Add(_mainContent);
-
-            _mainContentController = new MainContentController(_mainContent);
-            toolbar.requestRefresh = _mainContentController.RefreshLibrary;
-            toolbar.requestFilter = _mainContentController.SetTextFilter;
-
-            _mainContentController.Refresh();
         }
 
         
