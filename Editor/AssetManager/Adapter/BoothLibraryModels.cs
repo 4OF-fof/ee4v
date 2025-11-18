0980357c73be1102a56b9dd7d1bdf56ea32f6aaf
4OF <404.fof.nf@gmail.com>
2025-11-18 15:21:29 +0900

    refactor

--- Editor/AssetManager/Adapter/BoothLibraryModels.cs
+++ Editor/AssetManager/Adapter/BoothLibraryModels.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace _4OF.ee4v.AssetManager.Adapter {
@@ -27,5 +28,28 @@ namespace _4OF.ee4v.AssetManager.Adapter {
     [Serializable]
     public class ShopsWrapper {
         public List<ShopDto> Shops;
+    }
+    public static class BoothLibraryServerState {
+        private static readonly ConcurrentQueue<List<ShopDto>> Pending = new();
+        private static List<ShopDto> _lastContents = new List<ShopDto>();
+        public static IReadOnlyList<ShopDto> LastContents => _lastContents;
+
+        public static void SetContents(List<ShopDto> shops) {
+            // enqueue and let UI flush in EditorApplication.update
+            Pending.Enqueue(shops);
+        }
+
+        public static bool TryTakePending(out List<ShopDto> shops) {
+            if (Pending.TryDequeue(out shops)) {
+                _lastContents = shops ?? new List<ShopDto>();
+                return true;
+            }
+            shops = null;
+            return false;
+        }
+
+        public static void Clear() {
+            _lastContents = new List<ShopDto>();
+        }
     }
 }
