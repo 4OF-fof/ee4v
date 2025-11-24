0ec5f5d60a561db451336ad4793d2e7ab4ffa2a0
4OF <404.fof.nf@gmail.com>
2025-11-24 15:35:13 +0900

    fix

--- Editor/AssetManager/Utility/BoothUtility.cs
+++ Editor/AssetManager/Utility/BoothUtility.cs
@@ -32,11 +32,11 @@ namespace _4OF.ee4v.AssetManager.Utility {
             if (string.IsNullOrWhiteSpace(url)) return false;
 
             var m = Regex.Match(url,
-                @"^https?://(?:(?<sub>[^\.]+)\.booth\.pm|booth\.pm/(?<path>[^/]+))/items/(?<id>\d+)",
+                @"^https?://(?<sub>[^\.]+)\.booth\.pm/items/(?<id>\d+)",
                 RegexOptions.IgnoreCase);
             if (!m.Success) return false;
 
-            shopDomain = m.Groups["sub"].Success ? m.Groups["sub"].Value : m.Groups["path"].Value;
+            shopDomain = m.Groups["sub"].Value;
             itemId = m.Groups["id"].Value;
             return true;
         }
