2034fcca520d7555369d47d6329893e596b348d7
4OF <404.fof.nf@gmail.com>
2026-03-18 01:55:33 +0900

    Remove old files before importing new history

--- Editor/Core/Interfaces/IUtilityWindow.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _4OF.ee4v.Core.Interfaces {
-    public interface IEditorUtility {
-        string Name { get; }
-        string Description { get; }
-        string Trigger { get; }
-    }
-}
--- Runtime/AvatarModifyHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using VRC.SDKBase;
-
-namespace _4OF.ee4v.Runtime {
-    [DisallowMultipleComponent]
-    public class VariantAutoUpdater : MonoBehaviour, IEditorOnly {
-        // Dummy
-    }
-}
